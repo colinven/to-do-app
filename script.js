792d4829f0a52d4ce96cc8f26093b3021b8feca5
ColinVenancio <232087417+colinven@users.noreply.github.com>
2025-11-25 14:53:52 -0500

    added edit item feature, modified task input bar

--- script.js
+++ script.js
@@ -2,38 +2,105 @@ const inputBox = document.getElementById('input-box');
 const addButton = document.getElementById('add-btn');
 const listContainer = document.getElementById('list-container');
 const listItems = document.querySelectorAll('li');
+const editedItem = document.querySelector('edit')
+
+function toggleInputVisibility(){
+    if (inputBox.hidden){
+         inputBox.hidden = false;
+        addButton.innerText = 'Add';
+        addButton.classList.remove('inactive');
+    } else {
+        inputBox.hidden = true;
+        addButton.innerText = 'Add a Task';
+        addButton.classList.add('inactive');
+    }
+       
+}
+
+function createNewTaskElements(value){
+    //append list item
+    let li = document.createElement('li');
+    li.innerText = value.trim();
+    listContainer.appendChild(li);
+
+    //append edit button
+    let edit = document.createElement('span');
+    edit.innerHTML = '<img class="edit" src="./images/edit.png">'
+    li.appendChild(edit);
+
+    //append remove button
+    let remove = document.createElement('span');
+    remove.innerHTML = '<img class="remove" src="./images/remove.png">'
+    li.appendChild(remove);
+
+    return li;
+}
 
 function addTask(){
     if (inputBox.value === ''){
         alert('Please input your text.');
         return;
     }
-    let li = document.createElement('li');
-    li.innerText = inputBox.value;
-    listContainer.appendChild(li);
-    let span = document.createElement('span');
-    span.innerHTML = '<img src="./images/remove.png">'
-    li.appendChild(span);
+    createNewTaskElements(inputBox.value)
+    toggleInputVisibility();
     inputBox.value = '';
     saveData();
 }
+inputBox.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                addTask()
+            }
+        })
 
-addButton.addEventListener('click', addTask);
+addButton.addEventListener('click', function(){
+    if (inputBox.hidden) {
+        toggleInputVisibility();
+        inputBox.focus();
+    } else {
+        addTask();
+    }
+});
+
+//handle todo item states
 listContainer.addEventListener('click', event => {
     if (event.target.tagName === 'LI') {
         event.target.classList.toggle('checked');
         saveData();  
     }
     if (event.target.tagName === 'IMG') {
-      event.target.parentElement.parentElement.remove();
-      saveData();
-    }
-        
+        if (event.target.classList.contains('remove')) {
+            event.target.parentElement.parentElement.remove();
+            saveData();
+        }
+        if (event.target.classList.contains('edit')) {
+            const editField = editTask(event.target.parentElement.parentElement)
+            editField.addEventListener('keydown', (e) => {
+                if (e.key === "Enter"){
+                    saveEdit(editField);
+                    saveData(); 
+                }
+            })
+        }
+    }   
 });
 
+function editTask(listItem){
+    let input = document.createElement('input');
+    input.value = listItem.innerText;
+    input.id = 'edit';
+    listItem.replaceWith(input);
+    input.focus();
+    return input;
+}
+function saveEdit(inputElement){
+    const newValue = inputElement.value.trim();
+    const newListItem = createNewTaskElements(newValue);
+    inputElement.replaceWith(newListItem);
+}
 function saveData(){
     localStorage.setItem('data', listContainer.innerHTML)
 }
+
 function showTask(){
     listContainer.innerHTML = localStorage.getItem('data');
 }
